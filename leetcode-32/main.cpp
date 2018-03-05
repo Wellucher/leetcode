46b836c174bd69522c8bd9386757fc4903298a75
Yuchen Wong <595873286@qq.com>
2018-03-05 10:31:38 +0800

    Initial Commit

---
+++ leetcode-32/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode-32
+//
+//  Created by Chen on 2018/3/5.
+//  Copyright © 2018年 Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
